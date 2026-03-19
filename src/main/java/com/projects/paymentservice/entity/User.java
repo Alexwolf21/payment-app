bff653675421b29cb5ca830edd6c23ede43548c2
Auroshree Lenka <aurolenka2001@gmail.com>
2026-03-19 21:54:17 +0530

    Setup PostgreSQL Config

--- src/main/java/com/projects/paymentservice/entity/User.java
+++ src/main/java/com/projects/paymentservice/entity/User.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "payments")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
