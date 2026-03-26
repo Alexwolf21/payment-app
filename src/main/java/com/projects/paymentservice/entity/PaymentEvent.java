4949b74f2ed30328d5479eaac82621819fbed294
Auroshree Lenka <aurolenka2001@gmail.com>
2026-03-27 04:54:55 +0530

    Created Audit for Payments

---
+++ src/main/java/com/projects/paymentservice/entity/PaymentEvent.java
@@ -0,0 +1,38 @@
+package com.projects.paymentservice.entity;
+
+import com.projects.paymentservice.enums.PaymentEventType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payment_events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payment_id", nullable = true)
+    private Payment payment;
+
+    private Long userId;
+
+    private String idempotencyKey;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentEventType eventType;
+
+    @Column(length = 1000)
+    private String details;
+
+    private LocalDateTime createdAt;
+}
